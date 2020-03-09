688ae34091ed8cc33f86755ebbecf0c6940cbd73
konradglinka <konradglkm@gmail.com>
2020-03-09 13:37:32 +0100

    start projektu i poczatek kalendarza pylenia roślin

---
+++ src/main/java/Controller.java
@@ -0,0 +1,18 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Controller {
+    @FXML
+    TableView plantsTableView;
+
+    @FXML
+    void initialize()  {
+
+    }
+}
